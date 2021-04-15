04e510590d996b191a0349efa9c6cdd9b6cd3be6
Calderon Emilio Jesus <calderonemiliojesus@gmail.com>
2021-04-14 23:58:30 -0300

    Actualizacion de configuracion i3

--- i3/iniciarPC.sh
+++ i3/iniciarPC.sh
@@ -2,23 +2,28 @@
 
 #$( lxterminal -e "cal ;read; lxterminal" )
 
-#$( sleep 5 )
 
-$( i3-msg "workspace 2: - Internet" )
-$( i3-msg "exec firefox")
+#i3-msg "workspace 9:   Term;exec lxterminal"
+#i3-msg "exec lxterminal"
 
-$( i3-msg "workspace 1:   Terminal" )
+#$( sleep 1 )
+
+i3-msg "workspace 2: - Internet"
+i3-msg "exec firefox"
+
+i3-msg "workspace 1:   Terminal"
 
 #$( i3-msg exec lxterminal)
 #$( lxterminal )
 
-$( lxterminal -e "i3-msg move up;echo;cal;curl -s wttr.in | head -n 7;echo;date;read;ranger;lxterminal")
+lxterminal -e "i3-msg move up;sudo ntpd -qg;echo ;cal;curl -s wttr.in | head -n 7;echo;date;read;ranger;lxterminal"
 
 #$( i3-msg "move left" )
 
 
-$( lxterminal -e "i3-msg move left;lxterminal -e bashtop")
+lxterminal -e "i3-msg move left;lxterminal -e bashtop"
 
 
-$( i3-msg "exec telegram-desktop" )
+i3-msg "exec telegram-desktop"
+
 
